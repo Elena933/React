f7f098d2d957c187261c021c92ff6829dc2351dd
Elena933 <76497529+Elena933@users.noreply.github.com>
2023-09-23 23:12:42 -0400

    Delete PASV/ReactPASV/Kanbans/KanbanON-redux-axios directory

--- PASV/ReactPASV/Kanbans/KanbanON-redux-axios/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-
-import {applyMiddleware, legacy_createStore as createStore} from 'redux'
-import kanban from "./reducers";
-import {composeWithDevTools} from "redux-devtools-extension";
-import thunk from "redux-thunk";
-
-const store = createStore(kanban, composeWithDevTools(applyMiddleware(thunk)))
-export default store
